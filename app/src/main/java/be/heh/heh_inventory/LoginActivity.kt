988aabadc5143e2f734db2f0a662a6e4e7dc431d
DownoBE <donovanhoyois@hotmail.com>
2023-01-05 03:47:30 +0100

    - Register button - Additional error codes in ErrorCode.kt - A user can now register

--- app/src/main/java/be/heh/heh_inventory/LoginActivity.kt
+++ app/src/main/java/be/heh/heh_inventory/LoginActivity.kt
@@ -18,25 +18,28 @@ class LoginActivity : AppCompatActivity(){
 
         // UI Elements
         val loginButton = findViewById<Button>(R.id.login_button)
+        val registerButton = findViewById<Button>(R.id.register_button)
         val emailInput = findViewById<TextInputEditText>(R.id.input_email)
         val passwordInput = findViewById<TextInputEditText>(R.id.input_password)
 
         // Clicks listeners
         loginButton.setOnClickListener{
             when(checkInputs(emailInput, passwordInput)){
-                ErrorCode.EMAIL_FORMAT_INVALID -> Toast.makeText(this,
-                    R.string.error_mail_format, Toast.LENGTH_SHORT).show()
-                ErrorCode.PASSWORD_FORMAT_INVALID -> Toast.makeText(this,
-                    R.string.error_password_format, Toast.LENGTH_SHORT).show()
+                ErrorCode.EMAIL_FORMAT_INVALID -> Toast.makeText(this, R.string.error_mail_format, Toast.LENGTH_SHORT).show()
+                ErrorCode.PASSWORD_FORMAT_INVALID -> Toast.makeText(this, R.string.error_password_format, Toast.LENGTH_SHORT).show()
                 ErrorCode.OK -> when(tryToLogin(emailInput, passwordInput)){
-                    ErrorCode.USER_NOT_FOUND -> Toast.makeText(this,
-                        R.string.error_user_not_found, Toast.LENGTH_SHORT).show()
-                    ErrorCode.OK ->
-                        // Start the new activity and retrieve the permissions
-                        startActivity(Intent(this, HomeActivity::class.java)
-                            .putExtra("user_permission", DatabaseHelper.db.userDao().getUserPermissionByMail(emailInput.text.toString()))
-                            .putExtra("user_email", emailInput.text.toString())
-                            .apply{})
+                    ErrorCode.USER_NOT_FOUND -> Toast.makeText(this, R.string.error_user_not_found, Toast.LENGTH_SHORT).show()
+                    ErrorCode.OK -> confirmLogin(emailInput)
+                }
+            }
+        }
+        registerButton.setOnClickListener {
+            when(checkInputs(emailInput, passwordInput)){
+                ErrorCode.EMAIL_FORMAT_INVALID -> Toast.makeText(this, R.string.error_mail_format, Toast.LENGTH_SHORT).show()
+                ErrorCode.PASSWORD_FORMAT_INVALID -> Toast.makeText(this, R.string.error_password_format, Toast.LENGTH_SHORT).show()
+                ErrorCode.OK -> when(tryToRegister(emailInput, passwordInput)){
+                    ErrorCode.USER_ALREADY_EXISTS -> Toast.makeText(this, R.string.error_user_already_exists, Toast.LENGTH_SHORT).show()
+                    ErrorCode.OK -> confirmLogin(emailInput)
                 }
             }
         }
@@ -44,6 +47,7 @@ class LoginActivity : AppCompatActivity(){
         // Startup the db (and initialize if first startup)
         DatabaseHelper.startDatabase(applicationContext)
     }
+
     private fun checkInputs(emailInput : TextInputEditText, passwordInput : TextInputEditText) : Enum<ErrorCode>{
         // Retrieving text
         val emailText = emailInput.text.toString()
@@ -53,6 +57,7 @@ class LoginActivity : AppCompatActivity(){
         if (isEmpty(passwordText) || passwordText.length < 4) return ErrorCode.PASSWORD_FORMAT_INVALID
         return ErrorCode.OK
     }
+
     private fun tryToLogin(emailInput : TextInputEditText, passwordInput : TextInputEditText) : Enum<ErrorCode>{
         // Try to retrieve the user
         val user : User = DatabaseHelper.db.userDao().getByMail(emailInput.text.toString())
@@ -61,5 +66,21 @@ class LoginActivity : AppCompatActivity(){
             return ErrorCode.OK
         }
         return ErrorCode.USER_NOT_FOUND
+    }
+
+    private fun tryToRegister(emailInput: TextInputEditText, passwordInput: TextInputEditText): Enum<ErrorCode> {
+        val user : User = DatabaseHelper.db.userDao().getByMail(emailInput.text.toString())
+        if (user == null){
+            DatabaseHelper.db.userDao().insert(User(0, emailInput.text.toString(), BCrypt.hashpw(passwordInput.text.toString(), BCrypt.gensalt())))
+            return ErrorCode.OK
+        }
+        return ErrorCode.USER_ALREADY_EXISTS
+    }
+
+    private fun confirmLogin(emailInput: TextInputEditText) {
+        startActivity(Intent(this, HomeActivity::class.java)
+            .putExtra("user_permission", DatabaseHelper.db.userDao().getUserPermissionByMail(emailInput.text.toString()))
+            .putExtra("user_email", emailInput.text.toString())
+            .apply{})
     }
 }
--- app/src/main/java/be/heh/heh_inventory/data/ErrorCode.kt
+++ app/src/main/java/be/heh/heh_inventory/data/ErrorCode.kt
@@ -2,9 +2,10 @@ package be.heh.heh_inventory.data
 
 import be.heh.heh_inventory.R
 
-enum class ErrorCode(val errorText : String) {
-    EMAIL_FORMAT_INVALID(R.string.error_mail_format.toString()),
-    PASSWORD_FORMAT_INVALID(R.string.error_password_format.toString()),
-    USER_NOT_FOUND(R.string.error_user_not_found.toString()),
-    OK(R.string.error_ok.toString())
+enum class ErrorCode() {
+    EMAIL_FORMAT_INVALID(),
+    PASSWORD_FORMAT_INVALID(),
+    USER_NOT_FOUND(),
+    USER_ALREADY_EXISTS(),
+    OK(),
 }
