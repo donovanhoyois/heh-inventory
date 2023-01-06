a3871f257d523440680f500ca5dc91a4548b7d9e
DownoBE <donovanhoyois@hotmail.com>
2023-01-06 18:49:44 +0100

    - removed useless last_login in User.kt

--- app/src/main/java/be/heh/heh_inventory/database/user/User.kt
+++ app/src/main/java/be/heh/heh_inventory/database/user/User.kt
@@ -8,6 +8,5 @@ data class User(
     @PrimaryKey(autoGenerate = true) val uid: Long,
     @ColumnInfo(name = "user_mail_address") val email: String? = null,
     @ColumnInfo(name = "user_password") val password: String? = null,
-    @ColumnInfo(name = "user_permission") val permission: DatabasePermission = DatabasePermission.READ,
-    @ColumnInfo(name = "last_login") val lastLogin: String? = null){
+    @ColumnInfo(name = "user_permission") val permission: DatabasePermission = DatabasePermission.READ){
 }
